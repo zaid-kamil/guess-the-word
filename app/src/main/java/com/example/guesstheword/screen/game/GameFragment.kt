d678533c89c9f076837404af9c84d2bedfaa2616
zaid kamil <xaidmetamorphos@gmail.com>
2021-08-27 18:04:11 +0530

    viewmodel implemented # task 7 to be done

--- app/src/main/java/com/example/guesstheword/screen/game/GameFragment.kt
+++ app/src/main/java/com/example/guesstheword/screen/game/GameFragment.kt
@@ -1,11 +1,13 @@
 package com.example.guesstheword.screen.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.guesstheword.R
 import com.example.guesstheword.databinding.GameFragmentBinding
 
@@ -14,16 +16,8 @@ import com.example.guesstheword.databinding.GameFragmentBinding
  */
 class GameFragment : Fragment() {
 
-    // The current word
-    private var word = ""
-
-    // The current score
-    private var score = 0
-
-    // The list of words - the front of the list is the next word to guess
-    private lateinit var wordList: MutableList<String>
-
     private lateinit var binding: GameFragmentBinding
+    private lateinit var viewModel: GameViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,10 +31,8 @@ class GameFragment : Fragment() {
             container,
             false
         )
-
-        resetList()
-        nextWord()
-
+        Log.i("GameFragment", "Called ViewModelProvider.get")
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
         updateScoreText()
@@ -49,68 +41,23 @@ class GameFragment : Fragment() {
 
     }
 
-    /**
-     * Resets the list of words and randomizes the order
-     */
-    private fun resetList() {
-        wordList = mutableListOf(
-            "queen",
-            "hospital",
-            "basketball",
-            "cat",
-            "change",
-            "snail",
-            "soup",
-            "calendar",
-            "sad",
-            "desk",
-            "guitar",
-            "home",
-            "railway",
-            "zebra",
-            "jelly",
-            "car",
-            "crow",
-            "trade",
-            "bag",
-            "roll",
-            "bubble"
-        )
-        wordList.shuffle()
-    }
-
-    /** Methods for buttons presses **/
-
     private fun onSkip() {
-        score--
-        nextWord()
+        viewModel.onSkip()
+        updateScoreText()
+        updateWordText()
     }
 
     private fun onCorrect() {
-        score++
-        nextWord()
-    }
-
-    /**
-     * Moves to the next word in the list
-     */
-    private fun nextWord() {
-        if (!wordList.isEmpty()) {
-            //Select and remove a word from the list
-            word = wordList.removeAt(0)
-        }
+        viewModel.onCorrect()
         updateWordText()
         updateScoreText()
     }
 
-
-    /** Methods for updating the UI **/
-
     private fun updateWordText() {
-        binding.wordText.text = word
+        binding.wordText.text = viewModel.word
     }
 
     private fun updateScoreText() {
-        binding.scoreText.text = score.toString()
+        binding.scoreText.text = viewModel.score.toString()
     }
 }
